936e73ca28ef872b83d7135fd65884152d3fe895
2 <alvinlog@kaist.ac.kr>
2021-12-21 07:27:53 +0900

    PHP 기본

---
+++ PHP/function.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="UTF-8">
+  <title>function</title>
+</head>
+
+<body>
+  <h1>Function</h1>
+  <h2>Basic</h2>
+  <?php 
+    function basic(){
+      print("Lorem<br>");
+      print("Lorem is ");
+    }
+      basic();
+  ?>
+
+</body>
+
+</html>
